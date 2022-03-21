94c5781e25342c3b69b25aa877ad2318f4c67b5d
nilscox <nils@epita.fr>
2022-03-21 21:34:09 +0100

    react 18

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,10 @@ import ReactDOM from 'react-dom';
 
 import { App } from './App';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const root = ReactDOM.createRoot(document.getElementById('app'));
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
